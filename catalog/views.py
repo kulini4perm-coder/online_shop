48dc56dba9cc837d50a961342dec4ff4e8a2b4cd
Artem Kulinich <kulini4perm@gmail.com>
2026-04-15 17:09:37 +0500

    Add product detail controller, ORM queries and template inheritance

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'catalog/home.html', context)
 
 def contacts(request):
     if request.method == "POST":
@@ -11,3 +17,9 @@ def contacts(request):
 
         return HttpResponse(f'Спасибо, {name}! Сообщение получено.')
     return render(request, 'catalog/contacts.html')
+
+def product_detail(request, product_id):
+    # Получаем один товар по его id
+    product = Product.objects.get(id=product_id)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context=context)
